e7cefacdfbac4febc51e97d2936606ed4552f931
Cleginaldo <cleginaldo3000@gmail.com>
2024-06-23 16:21:10 -0300

    feat: add subjects and type infer with zod

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,12 @@
-import { ability } from '@saas/auth'
+import { defineAbilityFor } from '@saas/auth'
 import { log } from 'console'
+
+const ability = defineAbilityFor({ role: 'MEMBER' })
 
 const userCanInvite = ability.can('invite', 'User')
 const userCanDelete = ability.can('delete', 'User')
+const userCannotDelete = ability.cannot('delete', 'User')
 
 log(userCanInvite)
 log(userCanDelete)
+log(userCannotDelete)
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,27 +1,42 @@
 import {
   AbilityBuilder,
   CreateAbility,
-  ForcedSubject,
   MongoAbility,
   createMongoAbility,
 } from '@casl/ability'
+import { z } from 'zod'
+import { User } from './models/user'
+import { permissions } from './permissions'
+import { billingSubject } from './subjects/billing'
+import { inviteSubject } from './subjects/invite'
+import { organizationSubject } from './subjects/organization'
+import { projectSubject } from './subjects/project'
+import { userSubject } from './subjects/user'
 
-const actions = ['manage', 'invite', 'delete'] as const
-const subjects = ['User', 'all'] as const
-type AppAbilities = [
-  (typeof actions)[number],
-  (
-    | (typeof subjects)[number]
-    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
-  ),
-]
+const appAbilitySchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+  z.tuple([z.literal('manage'), z.literal('all')]),
+])
+
+type AppAbilities = z.infer<typeof appAbilitySchema>
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
 
-const { build, can, cannot } = new AbilityBuilder(createAppAbility)
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
 
-can('invite', 'User')
-cannot('delete', 'User')
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permissions for role ${user.role} not found`)
+  }
 
-export const ability = build()
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+
+  return ability
+}
