4b4ca3b8573f936fd7360ad287ceceb88fcceaff
Cleginaldo <cleginaldo3000@gmail.com>
2024-06-30 00:05:42 -0300

    saas: add create user routes with assossiation

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -10,6 +10,7 @@ export async function createAccount(app: FastifyInstance) {
     '/users',
     {
       schema: {
+        summary: 'Create new account',
         body: z.object({
           name: z.string(),
           email: z.string().email(),
@@ -32,6 +33,15 @@ export async function createAccount(app: FastifyInstance) {
           .send({ message: 'User with same e-mail already exists.' })
       }
 
+      const [, domain] = email.split('@')
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true,
+        },
+      })
+
       const passwordHash = await hash(password, 6)
 
       await prisma.user.create({
@@ -39,6 +49,13 @@ export async function createAccount(app: FastifyInstance) {
           name,
           email,
           passwordHash,
+          member_on: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                },
+              }
+            : undefined,
         },
       })
 
