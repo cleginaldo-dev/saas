9af67d3df3bb8f502be5671fb404d1c957ddb7f7
Cleginaldo <cleginaldo3000@gmail.com>
2024-07-20 23:15:41 -0300

    saas: add auth with github

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -16,6 +16,7 @@ import { getProfile } from '@/http/routes/auth/get-profile'
 import { requestPasswordRecover } from '@/http/routes/auth/request-password-recover'
 import { resetPassword } from '@/http/routes/auth/reset-password'
 
+import { authenticateWithGithub } from './routes/auth/authenticate-with-github'
 import { createAccount } from './routes/auth/create-account'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -49,6 +50,7 @@ app.register(fastifyCors)
 
 app.register(createAccount)
 app.register(authenticateWithPassword)
+app.register(authenticateWithGithub)
 app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
