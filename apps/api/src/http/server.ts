5c309fa189189ef5844545e233b13ee75cefc439
Cleginaldo <cleginaldo3000@gmail.com>
2024-07-26 01:13:02 -0300

    saas: add list org

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -21,6 +21,8 @@ import { createOrganization } from '@/http/routes/orgs/create-organization'
 
 import { createAccount } from './routes/auth/create-account'
 import { getMembership } from './routes/orgs/get-membership'
+import { getOrganization } from './routes/orgs/get-organization'
+import { getOrganizations } from './routes/orgs/get-organizations'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -68,6 +70,8 @@ app.register(resetPassword)
 
 app.register(createOrganization)
 app.register(getMembership)
+app.register(getOrganization)
+app.register(getOrganizations)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
