5a0b53bc41ac0870a823e32496e07cbfbfc7f993
Cleginaldo <cleginaldo3000@gmail.com>
2024-10-29 00:14:13 -0300

    saas: project web - create header organization switcher

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,6 +1,11 @@
+import { Slash } from 'lucide-react'
 import Image from 'next/image'
+
 import rocketseatIcon from '@/assets/rocketseat-icon.svg'
+
+import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
+
 export function Header() {
   return (
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
@@ -10,7 +15,12 @@ export function Header() {
           className="size-6 dark:invert"
           alt="Rocketseat"
         />
+
+        <Slash className="size-3 -rotate-[24deg] text-border" />
+
+        <OrganizationSwitcher />
       </div>
+
       <div className="flex items-center gap-4">
         <ProfileButton />
       </div>
